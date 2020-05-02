51f157a802b0c95466df6f2ba075243dd26bd6fa
Trang Tran <ttminhtrang09@gmail.com>
2020-05-02 10:33:13 +0300

    Use body-parser

--- myApp.js
+++ myApp.js
@@ -1,48 +1,69 @@
 
 var express = require('express');
 var app = express();
-console.log("Hello World")
-app.get('/', function(req,  res) {
-  res.send('Hello Express')
-})
+const bodyParser = require('body-parser')
 
 // --> 7)  Mount the Logger middleware here
 
 
 // --> 11)  Mount the body-parser middleware  here
-
+app.use(bodyParser.urlencoded({extended: false}))
 
 /** 1) Meet the node console. */
-
+console.log("Hello World")
 
 /** 2) A first working Express Server */
 
 
 /** 3) Serve an HTML file */
-
+app.get('/', function(req,  res) {
+  const homePath = __dirname + '/views/index.html'
+  res.sendFile(homePath)
+})
 
 /** 4) Serve static assets  */
-
+app.use(express.static(__dirname + '/public'))
+/** 7) Root-level Middleware - A logger */
+//  place it before all the routes !
+app.use(function(req, res, next) {
+  const logMessage = `${req.method} ${req.path} - ${req.ip}`
+  console.log(logMessage)
+  next()
+})
 
 /** 5) serve JSON on a specific route */
+app.get('/json', function(req, res) {
+  if (process.env.MESSAGE_STYLE === 'uppercase') {
+    res.json({"message": "HELLO JSON"})
+  }
+  res.json({"message": "Hello json"})
+})
 
 
 /** 6) Use the .env file to configure the app */
- 
- 
-/** 7) Root-level Middleware - A logger */
-//  place it before all the routes !
 
 
 /** 8) Chaining middleware. A Time server */
-
+app.get('/now', function(req, res, next) {
+  req.time = new Date().toString()
+  next()
+}, function(req, res) {
+  res.json({"time": req.time})
+})
 
 /** 9)  Get input from client - Route parameters */
+app.get('/:word/echo', function(req, res) {
+  const word = req.params.word
+  res.json({"echo": word})
+})
 
 
 /** 10) Get input from client - Query parameters */
 // /name?first=<firstname>&last=<lastname>
-
+app.route('/name').get(function(req, res) {
+  const {first, last} = req.query
+  res.json({"name": `${first} ${last}`})
+})
   
 /** 11) Get ready for POST Requests - the `body-parser` */
 // place it before all the routes !
